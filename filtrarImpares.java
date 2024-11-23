4a900d26d71d4cd18a22eb68da137c263bb620a5
Juan Cristian Llobera Vázquez <jlloberavazquez@cifpfbmoll.eu>
2024-11-23 21:08:56 +0100

    Create filtrarImpares.java

---
+++ filtrarImpares.java
@@ -0,0 +1,26 @@
+//funciona que 
+import java.util.Arrays;
+public class main {
+    public static void filtrarImpares(int[] array) {
+       int contador=0;
+       for(int elemento :array){
+           if(elemento % 2 !=0){
+               contador++;
+           }
+       }
+       int[] pares = new int[contador];
+       int j=0;
+       for(int i =0 ; i<array.length ;i++){
+           if(array[i] % 2 != 0){
+               pares[j]=array[i];
+               j++;
+           }
+       }
+       System.out.println(Arrays.toString(pares));
+    }
+    public static void main(String[] args){
+    int[] misNumeros = {1,2,3,4,5,6,7,8,9};
+    filtrarImpares(misNumeros);
+
+    }
+}
