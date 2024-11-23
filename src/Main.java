774220ba0329b880db5940c1b89292658d941b74
Juan Cristian Llobera Vázquez <jlloberavazquez@cifpfbmoll.eu>
2024-11-23 11:16:49 +0100

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void imprimirNumeros() {
+        for(int i=1 ; i<=100 ; i++){
+            System.out.println(i);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+       imprimirNumeros();
+
+    }
+}
